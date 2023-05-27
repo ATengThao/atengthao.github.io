70f856bae6095075da8301862bfb14f4ef9604dd
LittleBeasty <thao0726@umn.edu>
2023-05-26 21:20:58 -0500

    Attempting to fix gh-pages

--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ import { BrowserRoute, Routes, Route } from 'react-router-dom';
 import Home from "./pages/Home";
 import './css/index.css';
 import App from './App';
+import NavBar from './NavBar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
-		<App />
+		<NavBar />
 	</React.StrictMode>
 );
