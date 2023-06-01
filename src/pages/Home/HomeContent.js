929e2347c89f3231bd70687fdcb6f85b32376625
ATengThao <atengthao@gmail.com>
2023-06-01 03:15:55 -0500

    Carousel basics completed

--- src/pages/Home/HomeContent.js
+++ src/pages/Home/HomeContent.js
@@ -2,6 +2,7 @@ import { Outlet, Link } from "react-router-dom";
 
 import andyImage from "../../images/andythao.png";
 import stemfuseArcade from "../../images/stemfuse_arcade-logo-white.png";
+import game2048 from "../../images/2048_game.png";
 
 import useScript from "../../useScript";
 
@@ -10,6 +11,8 @@ const runCarouselPath = "scripts/Home/runCarousel.js";
 function HomeContent() {
 	const shakePhotoScript = useScript(shakePhotoPath);
 	const runCarouselScript = useScript(runCarouselPath);
+	const carouselHeight = "400px";
+	const carouselHeightHalved = (parseInt(carouselHeight)/2).toString() + "px";
 	return (
 		<>
 		<div className="container-fluid">
@@ -38,9 +41,9 @@ function HomeContent() {
 				</div>
 			</div>
 			<div>
-				<h1 className="text-center mx-5 mt-5 p-3 border-bottom border-danger border-3">Projects</h1>
+				<h1 className="text-center mx-5 mt-5 p-3 border-bottom border-danger border-3">Projects I've Done</h1>
 			</div>
-			<div id="HomeCarousel" className="carousel slide" data-bs-ride="carousel" style={{height: "400px"}}>
+			<div id="HomeCarousel" className="carousel slide" data-ride="carousel" style={{height: carouselHeight}}>
 				<ol className="carousel-indicators">
 					<li type="button" data-target="#HomeCarousel" data-slide-to="0" aria-label="Slide 1" aria-current="true" className="active"></li>
 					<li type="button" data-target="#HomeCarousel" data-slide-to="1" aria-label="Slide 2" aria-current="false"></li>
@@ -48,15 +51,25 @@ function HomeContent() {
 				<div className="carousel-inner h-100">
 					<div className="carousel-item active h-100">
 						<div className="d-grid justify-content-center h-100">
-							<img id="stemfuseLogo" src={stemfuseArcade} alt="Stemfuse Arcade" className="d-block w-75 h-50 m-5"></img>
+							<Link to="/gotgame">
+								<img id="stemfuseLogo" src={stemfuseArcade} alt="Stemfuse Arcade" className="d-block w-75 h-50 m-5 object-fit-contain"></img>
+							</Link>
 							<div className="h-50 text-center">
 								<h5>GOT GAME Competition Hall of Famer</h5>
 								<p className="h-25 text-center">Where I took part in multiple game-making competitions</p>
 							</div>
 						</div>
 					</div>
-					<div className="carousel-item">
-						<h1 className="text-center m-5">GOODBYE</h1>
+					<div className="carousel-item h-100">
+						<div className="d-grid justify-content-center h-100 m-0 g-0 p-0">
+							<Link to="/ai2048" className="" style={{height: carouselHeightHalved}}>
+								<img id="2048game" src={game2048} alt="2048 Game AI" className="d-grid w-100 h-auto object-fit-cover"></img>
+							</Link>
+							<div className="h-50 text-center">
+								<h5>AI Heuristic Implementation</h5>
+								<p className="h-25 text-center">Designing a heuristic to improve an existing AI for the 2048 game</p>
+							</div>
+						</div>
 					</div>
 				</div>
 				<button className="carousel-control-prev" type="button" data-target="#HomeCarousel" data-slide="prev">
@@ -68,14 +81,6 @@ function HomeContent() {
 					<span className="visually-hidden">Next</span>
 				</button>
 			</div>
-			{/* <Carousel>
-				<Carousel.Item>
-					<h1 className="text-center m-5">HELLO</h1>
-				</Carousel.Item>
-				<Carousel.Item>
-					<h1 className="text-center m-5">GOODBYE</h1>
-				</Carousel.Item>
-			</Carousel> */}
 			<div className="row my-5">
 				<div className="col-2" />
 				<div className="col-8 p-5 contentBackground">
